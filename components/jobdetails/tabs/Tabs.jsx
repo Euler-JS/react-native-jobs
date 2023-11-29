7765b40670670f6dd1dfecc605f857b77c052340
Euler-JS <joaoalbertojose96@gmail.com>
2023-11-29 08:09:44 +0200

    Detalhes de job finalizados

--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
 
-import styles from './tabs.style'
+import styles from './tabs.style';
+import { SIZES } from '../../../constants';
 
-const Tabs = () => {
+const TabButton = ({ name, activeTab, onHandleSearchType }) => {
   return (
-    <View>
-      <Text>Tabs</Text>
-    </View>
-  )
-}
+    <TouchableOpacity onPress={onHandleSearchType} style={styles.btn(name, activeTab)}>
+      <Text>{name}</Text>
+    </TouchableOpacity>
+  );
+};
 
-export default Tabs
+const Tabs = ({ tabs, activeTab, setActiveTab }) => {
+  return (
+    <View style={styles.container}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={{columnGap: SIZES.small / 2}}>
+        {tabs.map((item, index) => (
+          <TabButton
+            key={index}
+            name={item}
+            activeTab={activeTab}
+            onHandleSearchType={() => setActiveTab(item)}
+          />
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default Tabs;
